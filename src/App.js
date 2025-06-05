a0c66906cac191dfcd5b0d8e4fc3067edbd4e5b8
st17931 <st17931@gmail.com>
2025-06-05 20:04:24 +0530

    correcting stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import CustomNavbar from './components/Navbar';
+import Hero from './components/Hero';
+import TrustedBy from './components/TrustedBy';
+import Features from './components/Features';
+import Contact from './components/Contact';
+import Testimonials from './components/Testimonials';
+import Footer from './components/Footer';
+import Categories from './components/Categories';
+import Payment from './components/Payment';
+import Brand from './components/Brand';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CustomNavbar />
+      <Hero />
+      <Features />
+      <TrustedBy/>
+      <Categories/>
+      <Payment/>
+      <Brand/>
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   );
 }
-
 export default App;
